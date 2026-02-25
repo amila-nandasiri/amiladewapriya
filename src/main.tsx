26cea6f436bb6077bb7d88d06c8f553e2307998e
Amila Dewapriya Nandasiri <amiladnandasiri@gmail.com>
2026-02-25 23:15:05 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css'; // Make sure index.css is in the same 'src' folder
 
 createRoot(document.getElementById('root')!).render(
- <StrictMode>
-   <App />
- </StrictMode>,
-)
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
